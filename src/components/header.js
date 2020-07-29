0506c30f535b70f29c47fc78fc1c0ffdfa962d0d
FACTS  LIMITED <admin@factsafrica.com>
2020-07-29 13:38:48 +0300

    Add global styling

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = props => {
+return (
+    <React.Fragment>
+        <h4>This is a header</h4>
+    </React.Fragment>
+)
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/header'
+
+const Main = props => {
+return <div>
+    <h3>The Main Class</h3>
+    <Header />
+</div>
+}
+
+export default Main
