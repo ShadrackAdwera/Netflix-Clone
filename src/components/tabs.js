261f6e96800ddc47665b4c4f999c9e976a3a30b6
FACTS  LIMITED <admin@factsafrica.com>
2020-07-29 15:48:19 +0300

    handle state to highlight the active tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList } from 'react-tabs';
 import ImageDoor from '../components/tabs-navigation/tab-door';
 import DeviceImage from '../components/tabs-navigation/tab-devices';
@@ -6,19 +6,23 @@ import PriceImage from '../components/tabs-navigation/tab-price';
 import '../styles/TabsNav.css';
 
 const TabsComponent = (props) => {
+
+    const [tabIndex, setTabIndex] = useState(0)
+
+
   return (
     <div>
-      <Tabs className='tabs'>
+      <Tabs className='tabs' selectedIndex={tabIndex} onSelect={tabIndex=>setTabIndex(tabIndex)}>
         <TabList className='tab-nav-container'>
-          <Tab>
+          <Tab className={`${tabIndex === 0? 'active': null}`}>
             <ImageDoor />{' '}
             <p><strong>No commitments <br/> cancel online at anytime</strong></p>
           </Tab>
-          <Tab>
+          <Tab className={`${tabIndex === 1? 'active': null}`}>
             <DeviceImage />
             <p style={{marginTop:"-5.3125rem"}}><strong>Watch anywhere</strong></p>
           </Tab>
-          <Tab>
+          <Tab className={`${tabIndex === 2? 'active': null}`}>
             <PriceImage />
             <p><strong>Pick Your Price</strong></p>
           </Tab>
