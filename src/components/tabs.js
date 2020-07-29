5995b8b46a04c31b6b988ce3fb7092b875c9247b
FACTS  LIMITED <admin@factsafrica.com>
2020-07-29 15:34:24 +0300

    Center the nav items

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Tab, Tabs, TabList } from 'react-tabs';
-import ImageDoor from '../components/tabs-navigation/tab-door'
-import DeviceImage from '../components/tabs-navigation/tab-devices'
-import PriceImage from '../components/tabs-navigation/tab-price'
-import '../styles/TabsNav.css'
+import ImageDoor from '../components/tabs-navigation/tab-door';
+import DeviceImage from '../components/tabs-navigation/tab-devices';
+import PriceImage from '../components/tabs-navigation/tab-price';
+import '../styles/TabsNav.css';
 
 const TabsComponent = (props) => {
   return (
     <div>
-      <Tabs>
-        <TabList>
-          <Tab><ImageDoor/></Tab>
-          <Tab><DeviceImage/></Tab>
-          <Tab><PriceImage/></Tab>
+      <Tabs className='tabs'>
+        <TabList className='tab-nav-container'>
+          <Tab>
+            <ImageDoor />{' '}
+            <p><strong>No commitments <br/> cancel online at anytime</strong></p>
+          </Tab>
+          <Tab>
+            <DeviceImage />
+            <p style={{marginTop:"-5.3125rem"}}><strong>Watch anywhere</strong></p>
+          </Tab>
+          <Tab>
+            <PriceImage />
+            <p><strong>Pick Your Price</strong></p>
+          </Tab>
         </TabList>
       </Tabs>
     </div>
