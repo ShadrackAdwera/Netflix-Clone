4bfd4b0c5a07115c82ea3c027ad974e298635553
FACTS  LIMITED <admin@factsafrica.com>
2020-07-29 15:11:23 +0300

    REmove x overflow

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../components/header'
+import Tabs from '../components/tabs'
 
 const Main = props => {
 return <div>
     <Header />
+    <Tabs />
 </div>
 }
 
