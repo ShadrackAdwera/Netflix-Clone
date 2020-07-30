b8b49aebb88d3581efed9095b075c76507bc7156
FACTS  LIMITED <admin@factsafrica.com>
2020-07-30 07:50:34 +0300

    add styling to the footer ul and li

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,73 +1,101 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Footer = () => {
 return (
-    <div>
-        <span>Questions? <Link to='/'>Call +254-7130-65-110</Link></span>
+    <FootedContainer>
+        <span style={{marginLeft:'17%', fontSize:'1.125rem'}}>Questions? <Link>Call +254-7130-65-110</Link></span>
         <div className='footer-columns'>
         <ul>
             <li>
                 <Link>FAQ</Link>
             </li>
             <li>
-                <Link>Investor Relations</Link>
+                <Link to='/'>Investor Relations</Link>
             </li>
             <li>
-                <Link>Ways to watch</Link>
+                <Link to='/'>Ways to watch</Link>
             </li>
             <li>
-                <Link>Corporate Information</Link>
+                <Link to='/'>Corporate Information</Link>
             </li>
             <li>
-                <Link>Netflix Originals</Link>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <Link>Help Center</Link>
-            </li>
-            <li>
-                <Link>Jobs</Link>
-            </li>
-            <li>
-                <Link>Terms Of Use</Link>
-            </li>
-            <li>
-                <Link>Contact Us</Link>
+                <Link to='/'>Netflix Originals</Link>
             </li>
         </ul>
         <ul>
             <li>
-                <Link>Account</Link>
+                <Link to='/'>Help Center</Link>
             </li>
             <li>
-                <Link>Redeem Gift Cards</Link>
+                <Link to='/'>Jobs</Link>
             </li>
             <li>
-                <Link>Privacy</Link>
+                <Link to='/'>Terms Of Use</Link>
             </li>
             <li>
-                <Link>Speed Test</Link>
+                <Link to='/'>Contact Us</Link>
             </li>
         </ul>
         <ul>
             <li>
-                <Link>Media Center</Link>
+                <Link to='/'>Account</Link>
             </li>
             <li>
-                <Link>Buy Gift Cards</Link>
+                <Link to='/'>Redeem Gift Cards</Link>
             </li>
             <li>
-                <Link>Cookie Preferences</Link>
+                <Link to='/'>Privacy</Link>
             </li>
             <li>
-                <Link>Legal Notices</Link>
+                <Link to='/'>Speed Test</Link>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <Link to='/'>Media Center</Link>
+            </li>
+            <li>
+                <Link to='/'>Buy Gift Cards</Link>
+            </li>
+            <li>
+                <Link to='/'>Cookie Preferences</Link>
+            </li>
+            <li>
+                <Link to='/'>Legal Notices</Link>
             </li>
         </ul>
         </div>
-    </div>
+    </FootedContainer>
 )
 }
 
 export default Footer
+
+const FootedContainer = styled.footer`
+    background: var(--main-deep-dark);
+    padding-top: 10rem;
+    padding-bottom: 3rem;
+    color: #999;
+
+    .footer-columns {
+        width: 70%;
+        margin: 1rem auto 0;
+        font-size: 0.9rem;
+        overflow: auto;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+    }
+    ul li{
+        list-style: none;
+        line-height: 2.5;
+    }
+    a {
+        color: #999;
+    }
+    a:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/header'
 import Tabs from '../components/tabs'
+import Footer from '../components/footer'
 
 const Main = props => {
 return <div>
     <Header />
     <Tabs />
+    <Footer />
 </div>
 }
 
