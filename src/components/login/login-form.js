c9fa03d3bd1b66a91f9e63f3d46b675851088561
FACTS  LIMITED <admin@factsafrica.com>
2020-07-30 18:20:15 +0300

    add styling to the checkbox

--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
 return <FormContainer> 
@@ -15,8 +16,14 @@ return <FormContainer>
             <label>Password</label>
         </div>
         <div className='input-container'>
-            <a type='submit' href='/'>Sign In</a>
+            <Button type='submit' href='/'>Sign In</Button>
         </div>
+        <label className='checkbox-container'>
+            Remember me
+            <input type='checkbox' checked/>
+            <span className='checkmark'></span>
+        </label>
+        <Link to='/' className='need-help'>Need Help?</Link>
     </form>
 </div>
 </FormContainer>
@@ -51,7 +58,75 @@ const FormContainer = styled.div`
         border-radius: 0.25rem;
         height: 3rem;
         padding: 0.9rem 1.25rem 0;
-        
     }
+    form div label {
+        position: absolute;
+        top: 0.625rem;
+        left: 1.25rem;
+        pointer-events: none;
+        color: #8a8a8a;
+        font-size: 1rem;
+        transition: transform 150ms ease-out, font-size 150ms ease-out;
+    }
+
+    form div {
+        position: relative
+    }
+
+    input:focus ~ label {
+        top: 0.43375rem;
+        font-size: 0.7rem;
+    } 
+    input:focus {
+        outline: none;
+    }
+
+    .checkbox-container {
+        margin-left: 0.75rem;
+        padding-left: 1.875rem;
+        position: relative;
+        font-size: 0.9rem;
+        cursor: pointer;
+    }
+    .checkbox-container input {
+        display: none;
+    }
+    .checkbox-container .checkmark {
+        display: inline-block;
+        background: #454545;
+        width: 1.1rem;
+        height: 1.1rem;
+        left: 0;
+        top: 0;
+        border-radius: 0.1rem;
+        position: absolute;
+    }
+    .checkbox-container input:checked + .checkmark:after {
+        content: '';
+        position: absolute;
+        height: 0.25rem;
+        width: 0.625rem;
+        border-left: 2px solid #000;
+        border-bottom: 2px solid #000;
+        top: 25%;
+        left: 21%;
+        transform: rotate(-45deg)
+    }
+`;
+
+const Button = styled.button`
+    color: #fff;
+    background: rgba(229,9,0);
+    border: none;
+    outline: none;
+    padding: 0.8rem 1.3rem;
+    border-radius: 0.125rem;
+    font-size: 1rem;
+    text-align: center;
+    box-shadow: 0 1px 0 rgba(0,0,0,0.45);
+    transition: opacity .2s ease-in;
+    cursor: pointer;
+    text-decoration: none;
+    margin: 1rem 0;
 
 `;
